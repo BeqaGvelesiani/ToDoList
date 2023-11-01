c635b613d5bb7c63e0eb529a8b329010a8a9023d
BeqaGvelesiani <107481738+BeqaGvelesiani@users.noreply.github.com>
2023-11-01 13:20:37 +0400

    დფგ

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function render() {
 
     for (let D = 1; D < mInfo[`task${i}`].length; D++) {
       let text = mInfo[`task${i}`][D][0];
-      console.log(`task${i}` + " " + text);
+      //console.log(`task${i}` + " " + text);
 
       document.getElementById(`t${i}`).innerHTML += `
         <div class="sub_task bordered" id="subtask${i}_${D}">
@@ -101,14 +101,14 @@ function controls() {
 function addTask(i) {
   controls();
   mInfo[`task${i + 1}`] = [["", 0]];
-  console.log(mInfo);
+  //console.log(mInfo);
   render();
 }
 function DelTask(i) {
   controls();
   //console.log(i);
   delete mInfo[`task${i}`];
-  console.log(mInfo);
+  //console.log(mInfo);
   render();
 }
 function DelSubTask(i, d) {
@@ -130,14 +130,14 @@ function makeChanges() {
 
     task.addEventListener("keyup", () => {
       mInfo[`task${i}`][0][0] = task.value;
-      console.log(`task changed ${mInfo[`task${i}`][0][0]}`);
+      //console.log(`task changed ${mInfo[`task${i}`][0][0]}`);
     });
 
     for (let D = 1; D < mInfo[`task${i}`].length; D++) {
       let subtask = document.getElementById(`subtask${i}_${D}text`);
       subtask.addEventListener("keyup", () => {
         mInfo[`task${i}`][D][0] = subtask.value;
-        console.log(`subTask changed ${mInfo[`task${i}`][D][0]}`);
+        //console.log(`subTask changed ${mInfo[`task${i}`][D][0]}`);
       });
     }
   }
@@ -147,72 +147,117 @@ function makeChanges() {
 // |
 // |
 function check_If_Done() {
-  for (let i = 1; i < Object.keys(mInfo).length; i++) {
-    let task = document.getElementById(`task${i}check`);
-    let taskText = document.getElementById(`task${i}text`);
-    let taskN = mInfo[`task${i}`][0][1];
-    foo(
-      taskN,
-      taskText,
-      task,
-      mInfo[`task${i}`][0][1],
-      mInfo[`task${i}`].length
-    );
 
-    for (let D = 1; D < mInfo[`task${i}`].length; D++) {
-      let subtask = document.getElementById(`subtask${i}_${D}check`);
-      let subtasktext = document.getElementById(`subtask${i}_${D}text`);
+  for(let i=1; i<Object.keys(mInfo).length; i++){
+    if(mInfo[`task${i}`].length == 1){
+      console.log("hey")
+      foo(
+        mInfo[`task${i}`],
+        document.getElementById(`task${i}text`),
+        check,
+        length
+      );
 
-      subtask.addEventListener("mouseup", () => {
-        if (subtasktext.value == false) {
-        } else if (mInfo[`task${i}`][D][1] == 0) {
-          subtasktext.style.textDecoration = "line-through";
-          subtasktext.style.color = "green";
-          subtask.innerHTML = "<h2>✔️</h2>";
-          mInfo[`task${i}`][D][1] = 1;
-          //console.log(mInfo);
-          //console.log(tu)
-          taskN += mInfo[`task${i}`][D][1];
 
-          foo(
-            taskN,
-            taskText,
-            task,
-            mInfo[`task${i}`][D][1],
-            mInfo[`task${i}`].length
-          );
-          console.log("taskN = " + taskN);
-        } else {
-          subtasktext.style.textDecoration = "none";
-          subtasktext.style.color = "black";
-          subtask.innerHTML = "<h2></h2>";
-          mInfo[`task${i}`][D][1] = 0;
-          //console.log(mInfo)
-          taskN -= 1;
 
-          foo(
-            taskN,
-            taskText,
-            task,
-            mInfo[`task${i}`][D][1],
-            mInfo[`task${i}`].length
-          );
-          console.log("taskN = " + taskN);
-        }
-      });
 
-      if (mInfo[`task${i}`][D][1]) {
-        subtasktext.style.textDecoration = "line-through";
-        subtasktext.style.color = "green";
-        subtask.innerHTML = "<h2>✔️</h2>";
-      } else {
-        subtasktext.style.textDecoration = "none";
-        subtasktext.style.color = "black";
-        subtask.innerHTML = "<h2></h2>";
-      }
+
+
+
+
+
+
+
+
+    }else{
+      console.log("hello");
+
+
+
+
+
+
+
+
+
+
+
+
     }
   }
+
+
+
+
+
+
+  // for (let i = 1; i < Object.keys(mInfo).length; i++) {
+  //   let task = document.getElementById(`task${i}check`);
+  //   let taskText = document.getElementById(`task${i}text`);
+  //   let taskN = mInfo[`task${i}`][0][1];
+  //   foo(
+  //     taskN,
+  //     taskText,
+  //     task,
+  //     mInfo[`task${i}`][0][1],
+  //     mInfo[`task${i}`].length
+  //   );
+
+  //   for (let D = 1; D < mInfo[`task${i}`].length; D++) {
+  //     let subtask = document.getElementById(`subtask${i}_${D}check`);
+  //     let subtasktext = document.getElementById(`subtask${i}_${D}text`);
+
+  //     subtask.addEventListener("mouseup", () => {
+  //       if (subtasktext.value == false) {
+  //       } else if (mInfo[`task${i}`][D][1] == 0) {
+  //         subtasktext.style.textDecoration = "line-through";
+  //         subtasktext.style.color = "green";
+  //         subtask.innerHTML = "<h2>✔️</h2>";
+  //         mInfo[`task${i}`][D][1] = 1;
+  //         //console.log(mInfo);
+  //         //console.log(tu)
+  //         taskN += mInfo[`task${i}`][D][1];
+
+  //         foo(
+  //           taskN,
+  //           taskText,
+  //           task,
+  //           mInfo[`task${i}`][D][1],
+  //           mInfo[`task${i}`].length
+  //         );
+  //         console.log("taskN = " + taskN);
+  //       } else {
+  //         subtasktext.style.textDecoration = "none";
+  //         subtasktext.style.color = "black";
+  //         subtask.innerHTML = "<h2></h2>";
+  //         mInfo[`task${i}`][D][1] = 0;
+  //         //console.log(mInfo)
+  //         taskN -= 1;
+
+  //         foo(
+  //           taskN,
+  //           taskText,
+  //           task,
+  //           mInfo[`task${i}`][D][1],
+  //           mInfo[`task${i}`].length
+  //         );
+  //         console.log("taskN = " + taskN);
+  //       }
+  //     });
 }
+
+//       if (mInfo[`task${i}`][D][1]) {
+//         subtasktext.style.textDecoration = "line-through";
+//         subtasktext.style.color = "green";
+//         subtask.innerHTML = "<h2>✔️</h2>";
+//       } else {
+//         subtasktext.style.textDecoration = "none";
+//         subtasktext.style.color = "black";
+//         subtask.innerHTML = "<h2></h2>";
+//       }
+//     }
+//   }
+// }
 
 function foo(i, text, check, length) {
   if (i & (i == length)) {
